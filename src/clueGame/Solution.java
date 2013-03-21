bc9d667a8bb9918ac6a7abd3195412b9425aefd2
Kyle Tucker <ktucker@mymail.mines.edu>
2013-03-20 21:37:18 -0600

    GameAction Tests

--- src/clueGame/Solution.java
+++ src/clueGame/Solution.java
@@ -5,4 +5,18 @@ public String person;
 public String weapon;
 public String room;
 
+public void setSolution(String person, String weapon, String room){
+	this.person = person;
+	this.weapon = weapon;
+	this.room = room;
+}
+public String getSolutionRoom(){
+	return this.room;
+}
+public String getSolutionWeapon(){
+	return this.weapon;
+}
+public String getSolutionPerson(){
+	return this.person;
+}
 }
